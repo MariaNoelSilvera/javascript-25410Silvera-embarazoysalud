ad9ec5e76a15627251725b62188198c224d9e3dd
Maria Noel Silvera <noel.silvera@pedidosya.com>
2022-04-04 14:01:22 -0300

    Desafio fetch

---
+++ Ajax y fetch/script.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+//Busca sugerencias
+function buscarCanciones(term) {
+    fetch(`${apiURL}/suggest/${term}`).
+        then((res) => res.json()).
+        then((json) => {
+            showData(json);
+        });
+}
+
+//Muestra sugerencias en el DOM
+function showData(data) {
+    result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+            .map(
+                song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Ver letra</button>
+    </li>`
+            )
+            .join('')}
+    </ul>
+  `;
+}
+
+// Encontrar letra de la canción y mostrar en DOM
+function verLetra(artist, songTitle) {
+    fetch(`${apiURL}/v1/${artist}/${songTitle}`).
+        then((res) => res.json()).
+        then((data) => {
+            if (data.error) {
+                result.innerHTML = data.error;
+            } else {
+                const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+                result.innerHTML = `
+                <h2><strong>${artist}</strong> - ${songTitle}</h2>
+                <span>${lyrics}</span>
+            `;
+            }
+        });
+}
+// Mostrar sugerencias al hacer click en el botón
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const searchTerm = search.value.trim();
+    if (!searchTerm) {
+        alert('Por favor ingrese un artista o canción');
+    } else {
+        buscarCanciones(searchTerm);
+    }
+});
+
+// Mostrar letra al hacer click en el botón
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+        verLetra(artist, songTitle);
+    }
+});
